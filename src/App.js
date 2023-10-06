ed2f54c8a6d89b16a1ff06085a3620bd65b5a04d
ranupl <ranupl542011@gmail.com>
2023-10-06 16:12:00 +0530

    resume added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from "react";
-import Navbar from "./Navbar";
-import About from '../src/components/About'; 
-import Resume from '../src/components/Resume'; 
-import Project from '../src/components/Project'; 
-import Home from '../src/components/Home'; 
-import Skills from '../src/components/Skills'; 
-import { Routes, Route } from 'react-router-dom';
+  import React from "react";
+  import Navbar from "./Navbar";
+  import About from '../src/components/About'; 
+  import Project from '../src/components/Project'; 
+  import Home from '../src/components/Home'; 
+  import Skills from '../src/components/Skills'; 
+  import { Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div className="row">
-      <div className="col-3 nav-bg">
-        <Navbar />
+  const App = () => {
+    return (
+      <div className="row">
+        <div className="col-3 nav-bg">
+          <Navbar />
+        </div>
+        <div className="col-9">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/skills" element={<Skills />} />
+          </Routes>
+        </div>
+
       </div>
-      <div className="col-9">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/skills" element={<Skills />} />
-        </Routes>
-      </div>
+    )
+  }
 
-    </div>
-  )
-}
-
-export default App;
+  export default App;
